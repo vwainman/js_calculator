6f6e36dd32709e78e63911a689546cefe3225b08
Vincent Wainman <vincewainman@gmail.com>
2022-06-02 16:49:20 -0600

    create barebones with limited functionality

--- scripts.js
+++ scripts.js
@@ -44,3 +44,35 @@ function operate(operator, x, y) {
     }
 }
 
+const operators = ['+', '-', 'x', '÷']
+const units = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const parenthesisButtons = document.querySelectorAll("#calculator>#interface>.parenthesis");
+const operatorButtons = document.querySelectorAll("#calculator>#interface>.operator");
+const unitButtons = document.querySelectorAll("#calculator>#interface>.unit");
+const clearButtons = document.querySelectorAll("#calculator>#interface>.clear");
+const decimalButton = document.querySelector("#calculator>#interface>.decimal");
+const equalsButton = document.querySelector("#calculator>#interface>.equals")
+const equationDisplay = document.querySelector("#calculator>#display>#equation");
+const resultDisplay = document.querySelector("#calculator>#display>#result");
+
+let result = 0;
+
+for (const unitButton of unitButtons) {
+    unitButton.addEventListener("click", appendDisplayUnit);
+}
+for (const clearButton of clearButtons) {
+    clearButton.addEventListener("click", clearDisplay);
+}
+
+function appendDisplayUnit(e) {
+    equationDisplay.textContent += `${e.target.id}`;
+}
+
+function clearDisplay(e) {
+    if (e.target.id === 'AC') {
+        equationDisplay.textContent = "";
+    } else if (e.target.id === 'C') {
+        equationDisplay.textContent = equationDisplay.textContent.slice(0, -1);
+    }
+}
